165e72b0b01cbe25bbfa7dc8588c940e3c76954b
ylfyt <ayudi3521@gmail.com>
2022-08-13 12:59:35 +0700

    Remove Env

--- src/main/java/com/easy/authservice/controllers/HomeController.java
+++ src/main/java/com/easy/authservice/controllers/HomeController.java
@@ -1,18 +1,13 @@
 package com.easy.authservice.controllers;
 
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-  @Autowired
-  private Environment env;
-
   @GetMapping("/")
   public String hello() {
-    return env.toString();
+    return "Hello, World!";
   }
 }
